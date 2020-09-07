92a783421d4fc3691e4328bcf7121154f9de3f8f
Tanuj Sharma <tanuj.sharma@tothenew.com>
2020-09-07 15:04:24 +0530

    Initial Commit

---
+++ ffmpeg-avplayer/ffmpeg_avplayer.h
@@ -0,0 +1,19 @@
+//
+//  ffmpeg_avplayer.h
+//  ffmpeg-avplayer
+//
+//  Created by Tanuj Sharma on 07/09/20.
+//  Copyright © 2020 Tanuj Sharma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ffmpeg_avplayer.
+FOUNDATION_EXPORT double ffmpeg_avplayerVersionNumber;
+
+//! Project version string for ffmpeg_avplayer.
+FOUNDATION_EXPORT const unsigned char ffmpeg_avplayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ffmpeg_avplayer/PublicHeader.h>
+
+
